5e1eb0e6c0e217f8c39c52d83cea078760cdb309
Andreas Schmitz <andreas.schmitz@wheregroup.com>
2015-06-27 18:56:20 +0200

    fixed more sf 2.7 deprecations

--- src/FOM/ManagerBundle/Form/Type/TagboxType.php
+++ src/FOM/ManagerBundle/Form/Type/TagboxType.php
@@ -3,14 +3,14 @@
 namespace FOM\ManagerBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class TagboxType extends AbstractType
 {
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array());
     }
--- src/FOM/UserBundle/Form/Type/UserType.php
+++ src/FOM/UserBundle/Form/Type/UserType.php
@@ -5,7 +5,7 @@ namespace FOM\UserBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Doctrine\ORM\EntityRepository;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
@@ -58,7 +58,7 @@ class UserType extends AbstractType
         }
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'requirePassword' => true,
